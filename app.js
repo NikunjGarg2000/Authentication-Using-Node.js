ccd0316cd650de711f15e59771b55f69f9a25162
Nikunj Garg <nikunjgarg2000@gmail.com>
2022-07-24 18:28:29 +0530

    Added Letting user submit secrets functionality.

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -91,12 +93,24 @@ app.get("/register", function(req, res) {
 })
 
 app.get("/secrets", function(req, res) {
+  User.find({"secret": {$ne:null}}, function(err, foundUsers) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  })
+})
+
+app.get("/submit", function(req, res) {
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
-})
+});
 
 app.get("/logout", function(req, res) {
   req.session.destroy(function(err) {
@@ -136,6 +150,23 @@ app.post("/login", function(req, res) {
       passport.authenticate("local")(req, res, function() {
         res.redirect("/secrets");
       })
+    }
+  })
+})
+
+app.post("/submit", function(req, res) {
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user._id.toString(), function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function() {
+          res.redirect("/secrets");
+        });
+      }
     }
   })
 })
